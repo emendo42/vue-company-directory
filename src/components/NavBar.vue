29e36910b4afd17fa0686abd1845a71c9f7d402c
Erick <emendo42@stu.southtexascollege.edu>
2022-09-13 11:23:29 -0500

    added nav layout

---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<script setup>
+  import { ref } from 'vue'
+  const brand = ref('🏫 Fake Company Directory 🏫')
+</script>
+<template>
+  <nav>
+    <div class="wrapper">
+      <div class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </div>
+      <div class="menu">
+        <a href="#" class="menu-item">Departments</a>
+        <a href="#" class="menu-item">Settings</a>
+        <a href="#" class="menu-login">Logout</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="postcss">
+  nav {
+    @apply flex h-20  bg-blue-900 text-slate-300;
+    .wrapper {
+      @apply container mx-auto flex w-full items-center justify-between;
+      .brand {
+        &-title {
+          @apply text-2xl font-bold text-green-500;
+        }
+      }
+      .menu {
+        @apply flex gap-4;
+        &-item {
+          @apply rounded-md px-4 py-3 hover:bg-green-500 hover:text-slate-900;
+        }
+        &-login {
+          @apply rounded-md px-4 py-3 font-bold text-slate-900 bg-red-200 hover:bg-red-700 hover:text-slate-400;
+        }
+      }
+    }
+  }
+</style>
