9938db1ad6b2504def8e10a73aaed7c31092c6cc
Erick <emendo42@stu.southtexascollege.edu>
2022-09-13 14:07:43 -0500

    added cards

---
+++ src/components/MainCardsSingle.vue
@@ -0,0 +1,49 @@
+<script setup>
+  import { faker } from '@faker-js/faker'
+  const firstName = faker.name.firstName()
+  const lastName = faker.name.lastName()
+  const fullName = `${firstName} ${lastName}`
+  const selectcCard = () => {
+    console.log(`${fullName} selected`)
+  }
+</script>
+
+<template>
+  <div class="card" @click="selectCard">
+    <div class="card-image">
+      <img :src="faker.internet.avatar()" alt="" srcset="" />
+    </div>
+    <div class="card-details">
+      <p class="card-details-name">{{ fullName }}</p>
+      <p class="card-details-job">{{ faker.name.jobTitle() }}, {{ faker.name.jobArea() }}</p>
+      <!--<p class="card-details-email">{{ faker.internet.exampleEmail(firstName, lastName) }}</p>-->
+      <p class="card-details-quote">"{{ faker.lorem.paragraph() }}"</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+  .card {
+    @apply cursor-pointer overflow-hidden rounded-xl bg-slate-100 p-8 shadow-md transition-transform duration-200 hover:scale-110 hover:shadow-2xl hover:shadow-gray-500;
+    &-image {
+      img {
+        @apply mx-auto rounded-full object-contain;
+      }
+    }
+    &-details {
+      @apply flex flex-col gap-2 pt-6 text-center;
+      &-name {
+        @apply text-3xl font-bold text-black;
+      }
+      &-job {
+        @apply -mt-3 text-sm font-semibold text-green-700;
+      }
+      &-email {
+        @apply text-slate-900;
+      }
+      &-quote {
+        @apply pt-2 text-lg italic text-slate-400;
+      }
+    }
+  }
+</style>
