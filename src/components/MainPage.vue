429539a3ad62a7b032eea51ca044dc80461f9d9b
Erick <emendo42@stu.southtexascollege.edu>
2022-09-20 17:19:17 -0500

    updted import path files

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,14 +1,14 @@
 <script setup>
-  import MainSearch from './MainSearch.vue'
-  import MainCards from './MainCards.vue'
-  import MainPagination from './MainPagination.vue'
+  import MainSearch from '@/components/MainSearch.vue'
+  import MainCards from '@/components//MainCards.vue'
+  import MainPagination from '@/components//MainPagination.vue'
 </script>
 
 <template>
   <div class="wrapper">
     <MainSearch />
     <MainCards />
-    <MainPagination/>
+    <MainPagination />
   </div>
 </template>
 
